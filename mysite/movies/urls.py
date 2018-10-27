7303f8b40d516004986aa20a9e226575acb7fa39
Grant King <grantryan.king@gmail.com>
2018-10-27 04:04:36 -0700

    feat: New URLs
    Add URLs for class based detail and create views for Movie and Category models.

--- mysite/movies/urls.py
+++ mysite/movies/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import CollectionListView, CategoryDetailView
+from .views import CollectionListView, CategoryDetailView, MovieCreateView, CategoryCreateView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='movies-home'),
-    path('item/', views.item, name='movies-item' ),
+    path('movie/', views.item, name='movies-item' ),
     path('example/', views.example, name='movies-example'),
     path('collection/', CollectionListView.as_view(), name='collection-list'),
     path('category/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('category/new/', views.CategoryCreateView.as_view(), name='category-new'),
+    path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
+    path('movie/new/', views.MovieCreateView.as_view(), name='movie-new')
 ]
