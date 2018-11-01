40331ab0c660be94108c85e4ce27501720ce3b09
Grant King <grantryan.king@gmail.com>
2018-11-01 00:52:08 -0700

    feat: Alter urls, add new
    Alter list view urls to more accurately reflect the funciton of each.
    
    Add update and delete category detail views.

--- mysite/movies/urls.py
+++ mysite/movies/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from .views import CollectionListView, CategoryDetailView, MovieCreateView, CategoryCreateView
+from .views import CategoryDetailView, MovieCreateView, CategoryCreateView, CategoryListView, MovieListView, CategoryDeleteView, CategoryUpdateView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='movies-home'),
     path('movie/', views.item, name='movies-item' ),
     path('example/', views.example, name='movies-example'),
-    path('collection/', CollectionListView.as_view(), name='collection-list'),
+    path('categories/', CategoryListView.as_view(), name='category-list'),
     path('category/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
     path('category/new/', views.CategoryCreateView.as_view(), name='category-new'),
+    path('category/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category-update'),
+    path('category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
+    path('movies/', views.MovieListView.as_view(), name='movie-list'),
     path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
-    path('movie/new/', views.MovieCreateView.as_view(), name='movie-new')
+    path('movie/new/', views.MovieCreateView.as_view(), name='movie-new'),
 ]
