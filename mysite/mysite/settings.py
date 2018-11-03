66aa3386b163bfba2ee2b903ff41a729ac483351
Grant King <grantryan.king@gmail.com>
2018-11-02 17:55:31 -0700

    feat: Make secret key secret
    Move secret key to environment variable as suggested by docs in preparation for hosting on Heroku.
    Obviously there is a record of this key in particular so it needs to change. I will have to figure out how.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-&a6z_&%pm%f_jfi0unw8!*0ow049@2=$08!)@d+#529@dy8ra'
+SECRET_KEY = os.environ.get('SECRET_KEY_DM')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +124,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'movies-home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
